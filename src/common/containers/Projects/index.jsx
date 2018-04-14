e569f7da1ab45075f3ad8e1a9d958ad7a6dd5f61
pascalmartin <verrverr@hotmail.com>
2018-04-14 18:24:41 -0400

    Make project with const list

--- src/common/containers/Projects/index.jsx
+++ src/common/containers/Projects/index.jsx
@@ -16,6 +16,38 @@ type Props = {
 	isLinksLoaded: boolean
 }
 
+const projects = [
+	{
+		id: 1,
+		name: 'Greenpeace0',
+		icon: 'https://baconmockup.com/50/50',
+		desc: 'Sauver mon chat',
+		link: '/projects/1',
+		pourcentage: 10
+	}, {
+		id: 2,
+		name: 'Greenpeace0',
+		icon: 'https://baconmockup.com/50/50',
+		desc: 'Sauver mon chat',
+		link: '/projects/1',
+		pourcentage: 10
+	}, {
+		id: 3,
+		name: 'Greenpeace0',
+		icon: 'https://baconmockup.com/50/50',
+		desc: 'Sauver mon chat',
+		link: '/projects/1',
+		pourcentage: 10
+	}, {
+		id: 4,
+		name: 'Greenpeace0',
+		icon: 'https://baconmockup.com/50/50',
+		desc: 'Sauver mon chat',
+		link: '/projects/1',
+		pourcentage: 10
+	}
+]
+
 class Links extends Component<Props> {
 	componentDidMount () {
 		if (!this.props.isLinksLoaded) {
@@ -40,15 +72,6 @@ class Links extends Component<Props> {
 				{!isLinksLoaded ? (
 					<Loader active>Loading data...</Loader>
 				) : (
-					/* <Grid stackable>
-						<Grid.Column width={16}>
-							<List relaxed divided animated>
-								{_.map(links, (linkItem, i) => {
-									return <LinkItem key={i} {...linkItem} />
-								})}
-							</List>
-						</Grid.Column>
-					</Grid> */
 					<div>
 						<Header as='h2' icon textAlign='center'>
 							<Icon name='users' circular />
@@ -57,84 +80,13 @@ class Links extends Component<Props> {
 							</Header.Content>
 						</Header>
 						<Grid columns={3} divided>
-							<Grid.Row>
-								<Grid.Column width="3">
-									<Image avatar src='https://baconmockup.com/50/50' />
-								</Grid.Column>
-								<Grid.Column width="10">
-									<List.Content>
-										<List.Header as='a'>{'Fondation du CHUS'}</List.Header>
-										<List.Description>{'Pour les malades, plain de nouvelle machine'}</List.Description>
-									</List.Content>
-								</Grid.Column>
-								<Grid.Column width="3">
-									90 %
-								</Grid.Column>
-							</Grid.Row>
-
-							<Grid.Row>
-								<Grid.Column width="3">
-									<Image avatar src='https://baconmockup.com/50/50' />
-								</Grid.Column>
-								<Grid.Column width="10">
-									<List.Content>
-										<List.Header as='a'>{'Operation enfant soleil.'}</List.Header>
-										<List.Description>{'Pour les enfants.'}</List.Description>
-									</List.Content>
-								</Grid.Column>
-								<Grid.Column width="3">
-									5 %
-								</Grid.Column>
-							</Grid.Row>
-							<Grid.Row>
-								<Grid.Column width="3">
-									<Image avatar src='https://baconmockup.com/50/50' />
-								</Grid.Column>
-								<Grid.Column width="10">
-									<List.Content>
-										<List.Header as='a'>{'Albert LaTulipe'}</List.Header>
-										<List.Description>{'Sauver mon chat'}</List.Description>
-									</List.Content>
-								</Grid.Column>
-								<Grid.Column width="3">
-									2.5 %
-								</Grid.Column>
-							</Grid.Row>
-							<Grid.Row>
-								<Grid.Column width="3">
-									<Image avatar src='https://baconmockup.com/50/50' />
-								</Grid.Column>
-								<Grid.Column width="10">
-									<List.Content>
-										<List.Header as='a'>{'Veronika Ossi'}</List.Header>
-										<List.Description>{'Has not watched anything recently'}</List.Description>
-									</List.Content>
-								</Grid.Column>
-								<Grid.Column width="3">
-									2.5 %
-								</Grid.Column>
-							</Grid.Row>
-
+							{_.map(projects, (linkItem, i) => {
+								return <LinkItem key={i} {...linkItem} />
+							})}
 						</Grid>
 						<br/>
-						<Button positive floated='right'>Ajouter</Button>
+						<Button positive floated='right' as='a' href='/swipes'>Ajouter</Button>
 					</div>
-					/* <List>
-						<List.Item>
-							<Image avatar src='https://baconmockup.com/50/50' />
-							<List.Content>
-								<List.Header as='a'>{'Village de sherbrooke'}</List.Header>
-								<List.Description>{'Reconstruire un puit, un don pour une goute'}</List.Description>
-							</List.Content>
-						</List.Item>
-						<List.Item>
-							<Image avatar src='https://baconmockup.com/50/50' />
-							<List.Content>
-								<List.Header as='a'>{'Veronika Ossi'}</List.Header>
-								<List.Description>{'Has not watched anything recently'}</List.Description>
-							</List.Content>
-						</List.Item>
-					</List> */
 				)}
 			</div>
 		)
