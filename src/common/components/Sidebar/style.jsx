f117fdddb7b2e92006df23b1b0847c4579bd4f5a
lcharlebois <leandrecharlebois@gmail.com>
2018-04-14 11:01:12 -0400

    Update Logo

--- src/common/components/Sidebar/style.jsx
+++ src/common/components/Sidebar/style.jsx
@@ -12,9 +12,5 @@ export const StyledSidebar = styled(Sidebar)`
 		box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
 		padding: 20px;
 		text-align: center;
-	
-		img {
-			filter: invert(1);
-		}
 	}
 `
