b913cc276c882088997bf4ac52b07213161a1827
pascalmartin <verrverr@hotmail.com>
2018-04-14 23:16:45 -0400

    Working reverse proxy

--- src/server/index.js
+++ src/server/index.js
@@ -6,12 +6,14 @@ import 'babel-polyfill'
 import express from 'express'
 import fetch from 'isomorphic-fetch'
 import addMiddlewares from './middlewares'
-import httpProxy from 'http-proxy'
+// import httpProxy from 'http-proxy'
 
 import API from './api'
 import SSR from './ssr'
 
-var proxy = httpProxy.createProxyServer()
+var proxy = require('express-http-proxy')
+
+// var proxy = httpProxy.createProxyServer()
 
 global.fetch = fetch
 
@@ -20,10 +22,18 @@ const app: express$Application = express()
 // Add global middlewares
 addMiddlewares(app)
 
+app.use('/odata', proxy('http://a654mdkhmg6h-wua6.humbledonations.com'))
+
+/*
 app.all('/odata/*', function (req, res) {
 	console.log('redirecting to odata Server1')
-	proxy.web(req, res, {target: 'http://a654mdkhmg6h-wua6.humbledonations.com/', ignorePath: true, changeOrigin: true})
-})
+	proxy.web(req, res, {
+		target: 'http://a654mdkhmg6h-wua6.humbledonations.com',
+		pathRewrite: {
+			'/odata/': '/' // remove base path
+		},
+		changeOrigin: true})
+}) */
 
 // Add API
 app.use('/api', API)
