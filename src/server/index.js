9bc1bf90f21808c47e1d432dc12a8f9549254667
pascalmartin <verrverr@hotmail.com>
2018-04-14 22:59:14 -0400

    add odata proxy

--- src/server/index.js
+++ src/server/index.js
@@ -6,8 +6,12 @@ import 'babel-polyfill'
 import express from 'express'
 import fetch from 'isomorphic-fetch'
 import addMiddlewares from './middlewares'
+import httpProxy from 'http-proxy'
+
 import API from './api'
 import SSR from './ssr'
+
+var proxy = httpProxy.createProxyServer()
 
 global.fetch = fetch
 
@@ -15,6 +19,12 @@ const app: express$Application = express()
 
 // Add global middlewares
 addMiddlewares(app)
+
+app.all('/odata/*', function (req, res) {
+	console.log('redirecting to odata Server1')
+	proxy.web(req, res, {target: 'http://a654mdkhmg6h-wua6.humbledonations.com/', ignorePath: true, changeOrigin: true})
+})
+
 // Add API
 app.use('/api', API)
 // Add SSR
