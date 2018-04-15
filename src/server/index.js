9196f515c898d536ae2dd45809e3264aa2e49e20
pascalmartin <verrverr@hotmail.com>
2018-04-15 00:00:06 -0400

    Rewrite host

--- src/server/index.js
+++ src/server/index.js
@@ -22,7 +22,14 @@ const app: express$Application = express()
 // Add global middlewares
 addMiddlewares(app)
 
-app.use('/odata', proxy('http://a654mdkhmg6h-wua6.humbledonations.com'))
+app.use('/odata', proxy('http://a654mdkhmg6h-wua6.humbledonations.com', {
+	userResDecorator: function (proxyRes, proxyResData, userReq, userRes) {
+	  var data = proxyResData.toString('utf8')
+	  data = data.replace('http://a654mdkhmg6h-wua6.humbledonations.com/', 'http://localhost:5000/odata/')
+	  console.log(data)
+	  return data
+	}
+}))
 
 /*
 app.all('/odata/*', function (req, res) {
